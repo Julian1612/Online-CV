e82589b1d3c8215b9c7621618a7901e14eb856e1
jschneid <92728359+Julian1612@users.noreply.github.com>
2025-05-13 11:23:58 +0200

    a

--- script.js
+++ script.js
@@ -1,24 +1,2 @@
-const { createApp } = Vue;
-
-createApp({
-  data() {
-    return {
-      name: "Julian Schneider",
-      title: "Junior Softwareentwickler",
-      skills: ["JavaScript", "Python", "Vue.js", "AWS", "Docker"],
-    };
-  },
-  template: `
-    <div>
-      <h1>{{ name }}</h1>
-      <h2>{{ title }}</h2>
-
-      <div class="section">
-        <h3>Skills</h3>
-        <ul>
-          <li v-for="skill in skills" :key="skill">{{ skill }}</li>
-        </ul>
-      </div>
-    </div>
-  `,
-}).mount("#app");
+alert("Willkommen zurück auf Julian's 90er Homepage!");
+console.log("HTML4ever!");

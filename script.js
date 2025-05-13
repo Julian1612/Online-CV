6604faea45c2a5884b61e2b753b1fce9e40c733a
jschneid <92728359+Julian1612@users.noreply.github.com>
2025-05-13 11:05:24 +0200

    new file

---
+++ script.js
@@ -0,0 +1,10 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      greeting: "Hi, ich bin Julian!",
+      message: "Willkommen auf meinem Developer-Portfolio."
+    };
+  }
+}).mount("#app");
